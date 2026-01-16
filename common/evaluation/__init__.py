f88c3765097b9a9b8cc42e1085752e6ba74cf6f7
Mantis-Ryuji <ryuji0801yama@icloud.com>
2026-01-16 19:13:37 +0900

    policy decision

--- common/evaluation/__init__.py
+++ common/evaluation/__init__.py
@@ -1,6 +1,7 @@
 from .label_matching import compute_label_map, apply_map, save_aligned_centroids, verify_label_matching
 from .spatial_consistency_score import compute_scs
-from .pga import SphericalPGA1D
+from .tangent_pca import TangentPCA1D
+from .great_circle_pga import GreatCirclePGA1D
 
 
 __all__ = [
@@ -11,5 +12,6 @@ __all__ = [
     
     "compute_scs",
     
-    "SphericalPGA1D",
+    "TangentPCA1D",
+    "GreatCirclePGA1D",
 ]
